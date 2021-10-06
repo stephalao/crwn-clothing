d2c15016b22e9c65a335620f2bdb6656ed9635d6
Stephenson <salao@debt.com>
2021-10-05 21:06:16 -0400

    finish react router

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
-
-import HomePage from './pages/homepage/homepage.component';
+import { Route, Switch } from 'react-router-dom';
 
 import './App.css';
 
-class App extends React.Component {
-  render() {
-    return (
-      <div className='App'>
-        <HomePage />
-      </div>
-    );
-  }
+import HomePage from './pages/homepage/homepage.component';
+
+const HatsPage = () => (
+  <div>
+    <h1>Hats Page</h1>
+  </div>
+);
+
+function App() {
+  return (
+    <div>
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/hats' component={HatsPage} />
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
