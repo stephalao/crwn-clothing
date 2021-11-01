0809de472693c7696fb100b35499f765393b07d0
Stephenson Alao <salao@debt.com>
2021-11-01 17:40:37 -0400

    SignIn comp

--- src/App.js
+++ src/App.js
@@ -3,9 +3,11 @@ import { Route, Switch } from 'react-router-dom';
 
 import './App.css';
 
-import HomePage from './pages/homepage/homepage.component';
-import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
+
+import ShopPage from './pages/shop/shop.component';
+import HomePage from './pages/homepage/homepage.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 
 function App() {
   return (
@@ -13,8 +15,8 @@ function App() {
       <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
-        {/* <Route exact path='/hats' component={HatsPage} /> */}
         <Route path='/shop' component={ShopPage} />
+        <Route path='/signin' component={SignInAndSignUpPage} />
       </Switch>
     </div>
   );
