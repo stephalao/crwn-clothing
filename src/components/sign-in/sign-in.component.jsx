fefb8e4bbd2c2a44bd43f51de878c3529d5bd2f5
Stephenson Alao <salao@debt.com>
2021-11-10 17:20:19 -0500

    firebase google auth

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -50,9 +50,10 @@ class SignIn extends React.Component{
             value={this.state.password} 
             handleChange={this.handleChange}   
           />
-
-          <CustomButton type="submit" > Sign In </CustomButton>
-          <CustomButton onClick={signInWithGoogle} > Sign in with Google </CustomButton>
+          <div className="buttons">
+            <CustomButton type="submit" > Sign In </CustomButton>
+            <CustomButton onClick={signInWithGoogle} isGoogleSignIn> Sign in with Google </CustomButton>
+          </div>
         </form>
       </div>
     )
